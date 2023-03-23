7e9a5192741adb5c5c6fd3491b54904a676ba311
hakan efe <hakanforwork@gmail.com>
2023-03-23 17:57:37 +0200

    week6

---
+++ AndOperatorWithRanges/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("enter the current temp");
+int currentTemp = Int32.Parse(Console.ReadLine());
+
+if (currentTemp <= 0)
+{
+    Console.WriteLine("Freezing");
+}
+else if (currentTemp > 0 && currentTemp < 5)
+{
+    Console.WriteLine("cold");
+}
+else if (currentTemp >= 5 && currentTemp < 10)
+{
+    Console.WriteLine("chilly");
+}
+
+else if (currentTemp >= 10 && currentTemp < 15)
+{
+    Console.WriteLine("cool");
+}
+else if (currentTemp >= 15 && currentTemp < 28)
+{
+    Console.WriteLine("warm");
+}
+else
+{
+    Console.WriteLine("boiling hot");
+}
---
+++ Week6AndOrLogicalOperators/Program.cs
@@ -0,0 +1,25 @@
+﻿string username, password;
+Console.WriteLine("Enter the username");
+username = Console.ReadLine();
+
+Console.WriteLine("Enter the password");
+password = Console.ReadLine();
+
+if(username != "admin" || password != "admin1234")
+{
+    Console.WriteLine("invalid user credential");
+}
+else
+{
+    Console.WriteLine("welcome");
+}
+
+
+//if (username == "admin" && password == "admin1234")
+//{
+//    Console.WriteLine("Welcome");
+//}
+//else
+//{
+//    Console.WriteLine("invalid user credentials");
+//}
